f8e4eeda7b67d04dc591e4461e295f937173fc5a
lgz997 <1052935729@qq.com>
2019-11-13 16:19:03 +0800

    入住

--- src/main/java/com/yiyang/service/impl/CheckinServiceImpl.java
+++ src/main/java/com/yiyang/service/impl/CheckinServiceImpl.java
@@ -1,6 +1,37 @@
 package com.yiyang.service.impl;
 
-public class CheckinServiceImpl {
+import com.yiyang.dao.CheckinMapper;
+import com.yiyang.entity.Checkin;
+import com.yiyang.service.CheckinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("CheckinService")
+public class CheckinServiceImpl implements CheckinService {
+    @Autowired
+    private CheckinMapper checkinMapper;
+
+    @Override
+    public int add(Checkin checkin){
+        return  checkinMapper.insert(checkin);
+    }
+    @Override
+    public int delet(int id){
+        return  checkinMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int update(Checkin checkin) {
+        return checkinMapper.updateByPrimaryKey(checkin);
+    }
+
+    @Override
+    public ArrayList<Checkin> getCheckin() {
+        return (ArrayList<Checkin>) checkinMapper.selectByExample();
+    }
 
 
 }
