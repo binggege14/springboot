f1b2c1705b62d55e8bc6538488cdcf2d0228da12
lgz997 <1052935729@qq.com>
2019-11-15 14:32:47 +0800

    入住

--- src/main/java/com/yiyang/controller/TestController.java
+++ src/main/java/com/yiyang/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.yiyang.controller;
 
 import com.yiyang.entity.Care;
+import com.yiyang.entity.Checkin;
+import com.yiyang.entity.CheckinExample;
+import com.yiyang.service.CheckinService;
 import com.yiyang.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,16 +12,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
+
 @RestController
 public class TestController {
 
     @Autowired
     private UserService userService;
+    private CheckinService checkinService;
+
+    public TestController(CheckinService checkinService) {
+        this.checkinService = checkinService;
+    }
 
     @RequestMapping("/index")
     public ModelAndView test(){
         ModelAndView mv=new ModelAndView();
         mv.setViewName("hello");
+        return mv;
+    }
+
+    @RequestMapping("/text")
+    public ModelAndView text(){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("index");
         return mv;
     }
 
@@ -29,6 +47,20 @@ public class TestController {
         return "{\"uid\":\""+care.getId()+"\",\"uname\":\""+care.getName()+"\"}";
     }
 
+    @RequestMapping("/checkin")
+    public @ResponseBody String selectByExample(CheckinExample example){
+        List<Checkin> checkins= (List<Checkin>) checkinService.selectByExample(example);
+        Checkin checkin=new Checkin();
+//        checkins.add(checkin);
+        return "{\"id\":\""+checkin.getId()+"\",\"创建者\":\""+checkin.getCreateBy()+"\"}";
+
+    }
+
+    @RequestMapping("/ID")
+    public @ResponseBody String selectId(Integer id){
+        Checkin checkin=checkinService.selectByPrimaryKey(id);
+        return "{\"id\":\""+checkin.getId()+"\",\"创建者\":\""+checkin.getCreateBy()+"\"}";
+    }
 
 
 }
